d84d77c39dd2d5e32561836b475859255e96a207
Kai <gkai70264@gmail.com>
2022-09-14 20:19:40 -0400

    Add `intersection` to standard library

--- lmathlib.d
+++ lmathlib.d
@@ -1,6 +1,8 @@
 import std.stdio;
 import std.math;
+import std.array;
 import std.conv;
+import std.algorithm;
 import sal_shared_api;
 
 int lsin(SalmonInfo i)
@@ -10,8 +12,27 @@ int lsin(SalmonInfo i)
   return 0;
 }
 
+int lintersection(SalmonInfo i)
+{
+  string[] list1 = i.environ.env_lists[i.aA[0]];
+  string[] list2 = i.environ.env_lists[i.aA[1]];
+  string[] l;
+  string list3 = "";
+  foreach (string n; list1)
+  {
+    if (canFind(list2, n))
+      l ~= n;
+  }
+
+  list3 ~= join(l, " ") ~ "";
+  writeln(list3);
+  i.returnValue(list3, SalType.str);
+  return 0;
+}
+
 extern (C) int sal_lib_init(SalmonEnvironment env)
 {
   env.env_funcs["sin"] = &lsin;
+  env.env_funcs["intersection"] = &lintersection;
   return 0;
 }
