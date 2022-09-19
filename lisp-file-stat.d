a0e17f91796cd7b156ce3d96351c46eb21d72fe5
Kai <gkai70264@gmail.com>
2022-09-18 20:00:17 -0400

    Lisp file tool

---
+++ lisp-file-stat.d
@@ -0,0 +1,17 @@
+module lispstat;
+
+import std.stdio;
+import std.file;
+import std.conv;
+import std.string;
+
+void main() {
+  int files = 0;
+  foreach (string s ; dirEntries(".", SpanMode.depth)) {
+    if (s.endsWith(".asd")) {
+      files += 1;
+    }
+  }
+
+  writeln("There were \033[31;1m" ~ files.to!string ~ "\033[0m Lisp files in this directory.");
+}
