f5a16e7395de157db4f85a753369f19fb1d989c1
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-10-09 16:39:52 -0400

    Insert license header for interpreter

--- salinterp.d
+++ salinterp.d
@@ -1,3 +1,7 @@
+// Copyright 2022 Kai Daniel Gonzalez. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /* SALMON: A basic implementation of Common Lisp. */
 // (print "Hello, Salmon World!")
 
