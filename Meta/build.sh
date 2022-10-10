d462069e7349a7f3daabc43791795ea801a5e1ef
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-10-09 20:41:51 -0400

    Add helper build script

---
+++ Meta/build.sh
@@ -0,0 +1,6 @@
+# Copyright 2022 Kai Daniel Gonzalez. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+sudo ninja install -C builddir
+bash copy-success.sh
