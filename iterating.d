8774969cac409cd8087472260f62226ad1abe2df
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-10-10 16:43:26 -0400

    Add iterator functions

---
+++ iterating.d
@@ -0,0 +1,28 @@
+module iterating;
+
+// Copyright 2022 Kai Daniel Gonzalez. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import salinterp;
+import sal_auxlib;
+import sal_shared_api;
+
+import std.string : join;
+import std.stdio : writeln;
+
+SalmonValue until(string[] arg, SalmonEnvironment env) {
+    SalmonValue condition = quickRun(arg[1], env);
+    
+    string _body = arg[2];
+    while (condition.getValue() != "true") {
+        quickRun(_body, env);
+        condition = quickRun(arg[1], env);
+    }
+    return new SalmonValue();
+}
+
+int salIteratingTools(SalmonEnvironment env) {
+    saL_keyword(env, "until", &until);
+    return 0;
+}
