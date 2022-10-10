295d138ab498280cc7af8553d96db6d68d390347
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-10-09 20:41:25 -0400

    Add a helper script

---
+++ copy-success.sh
@@ -0,0 +1,7 @@
+# Copyright 2022 Kai Daniel Gonzalez. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# Copy the library
+
+sudo cp /usr/local/lib/libsalmon.so /lib/libsalmon.so
