2a6971b9960dbd99d17cd599e20d51c4e82e628b
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-11-19 15:06:43 -0500

    Fix copy script not working when it saves output to lib64

--- copy-success.sh
+++ copy-success.sh
@@ -4,4 +4,5 @@
 
 # Copy the library
 
-sudo cp /usr/local/lib/libsalmon.so /lib/libsalmon.so
+sudo cp /usr/local/lib64/libsalmon.so /lib/libsalmon.so >/dev/null || echo "Couldn't find lib64, trying lib"
+sudo cp /usr/local/lib/libsalmon.so /lib/libsalmon.so >/dev/null || echo "Could not find lib"
