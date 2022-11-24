1a5be6df06148b9fce4229c3dad33a0fe6938026
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-11-24 12:48:42 -0500

    Update script build.sh

--- Meta/build.sh
+++ Meta/build.sh
@@ -1,6 +1,6 @@
 # Copyright 2022 Kai Daniel Gonzalez. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
-
-sudo ninja install -C builddir
+meson build
+sudo ninja install -C build
 bash copy-success.sh
